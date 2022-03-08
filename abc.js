35c882aafaca1c7623564524a6c3fd4a99f4f71e
yungbaepark <ybpark54@gmail.com>
2022-03-08 14:33:58 +0900

    another change in abc.js

--- abc.js
+++ abc.js
@@ -1,12 +1,1 @@
 let globalVar = "global";
-function addFive(num) {
-  globalVar = "nonono";
-  function add(num) {
-    num = globalVar;
-    return num + "NONONO";
-  }
-  // globalVar='inFunctionl'
-  return add(num);
-}
-QQQQQQQQQQQQQQQQQQQ
-same bracnh i Github
